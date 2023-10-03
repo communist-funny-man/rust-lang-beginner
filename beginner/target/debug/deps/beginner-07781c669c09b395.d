cc796f10b492ddabcb1c20f970024892e6a19d70
communist-funny-man <danidezaprieto@gmail.com>
2023-10-03 13:14:34 +0200

    Initial commit

---
+++ beginner/target/debug/deps/beginner-07781c669c09b395.d
@@ -0,0 +1,5 @@
+C:\Users\Usuario\Desktop\rustlang\rust-lang-beginner\beginner\target\debug\deps\beginner-07781c669c09b395.rmeta: src\main.rs
+
+C:\Users\Usuario\Desktop\rustlang\rust-lang-beginner\beginner\target\debug\deps\beginner-07781c669c09b395.d: src\main.rs
+
+src\main.rs:
---
+++ beginner/target/debug/deps/beginner-6f7e032179182e9e.d
@@ -0,0 +1,5 @@
+C:\Users\Usuario\Desktop\rustlang\rust-lang-beginner\beginner\target\debug\deps\beginner-6f7e032179182e9e.rmeta: src\main.rs
+
+C:\Users\Usuario\Desktop\rustlang\rust-lang-beginner\beginner\target\debug\deps\beginner-6f7e032179182e9e.d: src\main.rs
+
+src\main.rs:
